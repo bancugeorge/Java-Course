812a6e6494822cbcec6e78473416ca3b8d3a0d8c
George <bancugeorge@hotmail.com>
2019-05-31 19:42:58 +0300

    Add files via upload

---
+++ homework/week2/src/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti primul numar: ");
+        int n1 = scanner.nextInt();
+        System.out.println("Introduceti al doilea numar: ");
+        int n2 = scanner.nextInt();
+        System.out.println("Introduceti operatia dorita: ");
+        String operation = scanner.next();
+
+        if (operation.equals("+")) {
+            System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+        }
+        else if (operation.equals("-")) {
+            System.out.println(n1 + " - " + n2 + " = " + (n1 - n2));
+        }
+        else if (operation.equals("*")) {
+            System.out.println(n1 + " * " + n2 + " = " + (n1 * n2));
+        }
+        else if (operation.equals("/")) {
+            System.out.println(n1 + " / " + n2 + " = " + (n1 / n2));
+        }
+        else System.out.println("Bad operation!");
+
+    }
+
+
+
+    }
