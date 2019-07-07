0960f4bedb7b94a0da6aa25e33f7ead7a229736a
Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>
2019-07-07 12:37:42 +0300

    Check homework 2

---
+++ src/main/java/week2/bookstore/Book.java
@@ -0,0 +1,16 @@
+package week2.bookstore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Book {
+
+
+
+}
